aa4df159223ccd7ac4eee414eba7691a12ca78c3
rocky1996 <2791752775@qq.com>
2020-05-28 18:24:04 +0800

    初次接触go协程

---
+++ goroutines.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printHello()  {
+	time.Sleep(15 * time.Millisecond)
+	fmt.Println("Hello World!!!!")
+}
+
+func main()  {
+	fmt.Println("main execution started")
+	go printHello()
+	time.Sleep(10 * time.Millisecond)
+
+	fmt.Println("main execution stopped")
+}
